8866c543c552ca878ade7149ca35d6f3aee31cb0
是李啊！ <2479336223@qq.com>
2022-10-09 17:08:03 +0800

    修改读取static下文件的方式

--- src/main/java/com/autumn/weather/controller/AvatarDownloadController.java
+++ src/main/java/com/autumn/weather/controller/AvatarDownloadController.java
@@ -1,13 +1,16 @@
 package com.autumn.weather.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URLEncoder;
 
 @Slf4j
@@ -18,32 +21,30 @@ public class AvatarDownloadController {
     @RequestMapping("/avatarDownload")
     public void avatarDownload(HttpServletResponse response) {
         File avatar = null;
-        FileInputStream in = null;
+        InputStream in = null;
         OutputStream out = null;
         try {
-            avatar = ResourceUtils.getFile("src/main/resources/static/avatar/avatar.jpg");
             response.setHeader("content-disposition","attachment;filename=" + URLEncoder.encode("avatar.jpg", "UTF-8"));
-            in = new FileInputStream(avatar);
+            ClassPathResource classPathResource = new ClassPathResource("static//avatar/avatar.jpg");
+            in = classPathResource.getInputStream();
             out = response.getOutputStream();
             byte buffer[] = new byte[1024];
             int len = 0;
-            log.info("开始读取！！！----------start");
+            log.info("读取开始啦！！！");
             while ((len = in.read(buffer)) > 0) {
                 out.write(buffer, 0, len);
             }
-            log.info("读取结束！！！----------end");
+            log.info("读取结束啦！！！");
         } catch (IOException e) {
-            log.error("读取出错啦！！！----------start");
+            log.error("读取出错啦！！！");
             e.printStackTrace();
-            log.error("读取出错啦！！！----------end");
         } finally {
             try {
                 in.close();
                 out.close();
             } catch (IOException e) {
-                log.error("流关闭出错啦！！！----------start");
+                log.error("流关闭出错啦！！！");
                 e.printStackTrace();
-                log.error("流关闭出错啦！！！----------end");
             }
 
         }
